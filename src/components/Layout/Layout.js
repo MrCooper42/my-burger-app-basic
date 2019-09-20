75b94330381a62b43fd4ea00782b32670f98e569
MrCooper42 <mr.cooper42co@gmail.com>
2019-09-19 22:36:46 -0600

    mobile side drawer added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, {Component} from "react";
 import {Aux} from "../../higherOrderComponent/Auxillary";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import Toolbar from "../Toolbar/Toolbar";
 import classes from "./Layout.module.css";
 
-export const Layout = (props) => (
-  <Aux>
-    <Toolbar/>
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+export class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  };
+  
+  sideDrawerClosedHandler = () => {
+    this.setState(({showSideDrawer: false}));
+  };
+  
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return {
+        showSideDrawer: !prevState.showSideDrawer,
+      };
+    });
+  };
+  
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    );
+  }
+}
--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Logo from "../Logo/Logo";
 import NavigationItems from "../Navigation/NavigationItems/NavigationItems";
+import DrawerToggle from "../Navigation/SideDrawer/DrawerToggle/DrawerToggle";
 import classes from "./Toolbar.module.css";
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
-    <Logo/>
-    <NavigationItems/>
+    <DrawerToggle clicked={props.drawerToggleClicked}/>
+    <div className={classes.Logo}>
+      <Logo/>
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems/>
+    </nav>
   </header>
 );
 
